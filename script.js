5d243d25e968121218191e31690a278895b857c5
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-06-01 14:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function name() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),1000)
+})
+}
+
+name.then(res=>{
+	document.getElementById("output").innerText = res
+})
